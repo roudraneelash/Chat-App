edee8f2180c4a766ce07c96b272d318ca1ccfd2c
Roudraneel ash <37204484+roudraneelash@users.noreply.github.com>
2023-04-28 00:05:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.css";
-import App from "./app";
+import App from "./App";
 
 const root = document.getElementById("root");
 const el = ReactDOM.createRoot(root);
